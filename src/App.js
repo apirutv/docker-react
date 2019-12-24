8ce5806b6f851b3eba5ce266bdfe71da57455b4f
Apirut Vanchaam <apirut@Apiruts-MacBook-Pro.local>
2019-12-24 21:56:47 +0700

    changed app text

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          Hello React World !!!
+          Hello React World !!! (changed in feature)
         </p>
         <a
           className="App-link"
